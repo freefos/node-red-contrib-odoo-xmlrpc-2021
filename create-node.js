7cc49efd822e5e14e3513c3a5362dcb231914973
freefos <121096589+freefos@users.noreply.github.com>
2025-05-14 15:57:34 +0200

    Update create-node.js
    async/await

--- create-node.js
+++ create-node.js
@@ -1,36 +1,43 @@
 module.exports = function (RED) {
-    var handle_error = function(err, node) {
-        node.log(err.body);
-        node.status({fill: "red", shape: "dot", text: err.message});
+    const handle_error = function (err, node) {
+        node.log(err.body || err.message);
+        node.status({ fill: "red", shape: "dot", text: err.message });
         node.error(err.message);
     };
 
     function OdooXMLRPCCreateNode(config) {
         RED.nodes.createNode(this, config);
         this.host = RED.nodes.getNode(config.host);
-        var node = this;
+        const node = this;
 
-        node.on('input', function (msg) {
+        node.on('input', async function (msg) {
             node.status({});
-            this.host.connect(function(err, odoo_inst) {
-                if (err) {
-                    return handle_error(err, node);
+
+            try {
+                const odoo_inst = await node.host.connect();
+
+                const model = config.model;
+                const payload = msg.payload;
+
+                if (typeof payload !== 'object' || Array.isArray(payload)) {
+                    throw new Error("msg.payload must be an object representing record fields");
                 }
 
-                var inParams = [];
-                inParams.push(msg.payload);
-                var params = [];
-                params.push(inParams);
-                //node.log('Creating object for model "' + config.model + '"...');
-                odoo_inst.execute_kw(config.model, 'create', params, function (err, value) {
-                    if (err) {
-                        return handle_error(err, node);
-                    }
-                    msg.payload = value;
-                    node.send(msg);
-                });
-            });
+                // `create` expects a single object or list of objects
+                const args = [[payload]];
+
+                node.log(`Creating record in model "${model}" with values: ${JSON.stringify(payload)}`);
+
+                const result = await odoo_inst.execute_kw(model, 'create', args);
+
+                msg.payload = result;
+                node.status({ fill: "green", shape: "dot", text: "Record created" });
+                node.send(msg);
+            } catch (err) {
+                handle_error(err, node);
+            }
         });
     }
+
     RED.nodes.registerType("odoo-xmlrpc-create", OdooXMLRPCCreateNode);
 };
