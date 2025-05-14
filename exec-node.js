341c24af5a85ee404e6023dac3cf36307efdd693
freefos <121096589+freefos@users.noreply.github.com>
2025-05-14 15:35:29 +0200

    Update exec-node.js

--- exec-node.js
+++ exec-node.js
@@ -1,42 +1,50 @@
-module.exports = function (RED) {
-  function isDefinedValue(v) {
-    return !(v == null || typeof v === 'undefined');
-  }
+function isDefinedValue(v) {
+  return !(v == null || typeof v === 'undefined');
+}
 
+module.exports = function (RED) {
   function OdooXMLRPCExecNode(config) {
     RED.nodes.createNode(this, config);
-    const node = this;
     this.host = RED.nodes.getNode(config.host);
+    const node = this;
 
     node.on('input', async function (msg, send, done) {
       node.status({});
       send = send || function () { node.send.apply(node, arguments); };
 
       try {
-        const odoo = await node.host.connect(); // host.connect() now resolves to odoo instance
+        const odoo_inst = await node.host.connect(); // host.connect must return a Promise
 
         const method = msg.method || config.method;
         const model = config.model;
 
         const args = msg.payload;
         if (!Array.isArray(args)) {
-          throw new Error("msg.payload must be an array of arguments");
+          throw new Error("msg.payload must be an array (even if only one argument)");
         }
 
         const kwargs = msg.kwargs || {};
 
         node.log(`Calling ${model}.${method} with args: ${JSON.stringify(args)} and kwargs: ${JSON.stringify(kwargs)}`);
 
-        const result = await odoo.execute_kw(model, method, args, kwargs);
+        const value = await odoo_inst.execute_kw(model, method, args, kwargs);
 
-        msg.payload = result;
-        node.status({ fill: "green", shape: "dot", text: `${method} executed` });
+        msg.payload = value;
+
+        if (value === true) {
+          node.status({ fill: "green", shape: "dot", text: `'${method}' executed` });
+        }
+
         send(msg);
-        done();
+        if (done) done();
       } catch (err) {
         node.log(err.body || err.message);
         node.status({ fill: "red", shape: "dot", text: err.message });
-        done(err);
+        if (done) {
+          done(err);
+        } else {
+          node.error(err, err.message);
+        }
       }
     });
   }
